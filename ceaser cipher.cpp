f0a2e2b197f0a94f645620b1e6983c5ca3429b1b
NagaBabu30 <132924900+NagaBabu30@users.noreply.github.com>
2024-03-02 09:34:32 +0530

    Update ceaser cipher.cpp

--- ceaser cipher.cpp
+++ ceaser cipher.cpp
@@ -1,31 +1,49 @@
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 
-void caesarCipher(char text[], int key) {
-    for (int i = 0; text[i] != '\0'; i++) {
-        if (isalpha(text[i])) {
-            char offset = isupper(text[i]) ? 'A' : 'a';
-            printf("%c", ((text[i] - offset + key) % 26) + offset);
-        } else {
-            printf("%c", text[i]);
-        }
+int main()
+{
+    char plain[10], cipher[10];
+    int key, i, length;
+
+    printf("\n Enter the plain text:");
+    scanf(" %s", plain);  
+    printf("\n Enter the key value:");
+    scanf("%d", &key);
+
+    printf("\n \n \t PLAIN TEXT: %s", plain);
+    printf("\n \n \t ENCRYPTED TEXT: ");
+
+    for (i = 0, length = strlen(plain); i < length; i++)
+    {
+        cipher[i] = plain[i] + key;
+        
+        if (isupper(plain[i]) && (cipher[i] > 'Z'))
+            cipher[i] -= 26;
+        if (islower(plain[i]) && (cipher[i] > 'z'))
+            cipher[i] -= 26;
+
+        printf("%c", cipher[i]);
     }
+
+    printf("\n \n \t AFTER DECRYPTION : ");
+
+    for (i = 0; i < length; i++)
+    {
+        plain[i] = cipher[i] - key;
+
+        if (isupper(cipher[i]) && (plain[i] < 'A'))
+            plain[i] += 26;
+        if (islower(cipher[i]) && (plain[i] < 'a'))
+            plain[i] += 26;
+
+        printf("%c", plain[i]);
+    }
+
     printf("\n");
-}
-
-int main() {
-    char text[100];
-    printf("Enter the text to encrypt: ");
-    fgets(text, sizeof(text), stdin);
-
-    printf("Caesar Cipher Encryption:\n");
-    for (int key = 1; key <= 25; key++) {
-        printf("Key %d: ", key);
-        caesarCipher(text, key);
-    }
-
+    
     return 0;
 }
-
 
 
